ae84050aa32417a710ee75a6acc8d930011b29e9
Matt <MattMcCullough@utexas.edu>
2025-09-21 04:14:37 -0700

    Spots project changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
+import "./index.css";
 import {
   enableValidation,
   validationConfig,
   resetValidation,
   disableButton,
-} from "./validation.js";
+} from "../scripts/validation.js";
 
 const initialCards = [
   {
